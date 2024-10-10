79886969a5c385e89d42158974dc2a139ca13c96
vikenty19 <vikenty.plakhov@testpro.io>
2024-10-10 16:11:34 -0400

    loginSuccess added

--- src/test/java/tutorial/tests/LoginClass.java
+++ src/test/java/tutorial/tests/LoginClass.java
@@ -1,9 +1,13 @@
 package tutorial.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -20,7 +24,20 @@ public class LoginClass {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
          driver.get(URL);
-       
+         driver.findElement(By.cssSelector(".fa-user")).click();
+        WebElement login = wait.until(ExpectedConditions
+                .elementToBeClickable(By.linkText("Login")));
+        login.click();
+        WebElement email = wait.until(ExpectedConditions
+                .elementToBeClickable(By.cssSelector("#input-email")));
+        email.clear();
+        email.sendKeys("vv@mail.ru");
+        driver.findElement(By.cssSelector("#input-password")).sendKeys("megadelta");
+        driver.findElement(By.xpath("//input[@type ='submit']")).click();
+        WebElement account =wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.xpath("//h2[text() ='My Account']")));
+        Assert.assertTrue((account.isDisplayed()));
+        driver.quit();
 
     }
 
