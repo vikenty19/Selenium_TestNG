1f75f8197cdbf4a6f748bdc51852540257914e3d
vikenty19 <vikenty.plakhov@testpro.io>
2024-10-10 16:24:55 -0400

    JavaFaker and BasePage added

---
+++ src/test/java/com/tutorial/BasePage.java
@@ -0,0 +1,9 @@
+package com.tutorial;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+  protected  WebDriver driver;
+  protected WebDriverWait wait;
+}
--- src/test/java/tutorial/tests/LoginClass.java
+++ src/test/java/tutorial/tests/LoginClass.java
@@ -1,5 +1,6 @@
 package tutorial.tests;
 
+import com.tutorial.BasePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,19 +13,20 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginClass {
+public class LoginClass extends BasePage {
     String URL = "http://tutorialsninja.com/demo/";
+
     @Test
-    public void loginSuccess(){
+    public void loginSuccess() {
 
 
         WebDriverManager.chromedriver().setup();
-      WebDriver driver=new ChromeDriver();
-      driver.manage().window().maximize();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-         driver.get(URL);
-         driver.findElement(By.cssSelector(".fa-user")).click();
+        driver.get(URL);
+        driver.findElement(By.cssSelector(".fa-user")).click();
         WebElement login = wait.until(ExpectedConditions
                 .elementToBeClickable(By.linkText("Login")));
         login.click();
@@ -34,7 +36,7 @@ public class LoginClass {
         email.sendKeys("vv@mail.ru");
         driver.findElement(By.cssSelector("#input-password")).sendKeys("megadelta");
         driver.findElement(By.xpath("//input[@type ='submit']")).click();
-        WebElement account =wait.until(ExpectedConditions
+        WebElement account = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(By.xpath("//h2[text() ='My Account']")));
         Assert.assertTrue((account.isDisplayed()));
         driver.quit();
