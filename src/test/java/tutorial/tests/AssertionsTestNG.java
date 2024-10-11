5210e24f6a83092fceffc4fa89a77242ee0ab24a
vikenty19 <vikenty.plakhov@testpro.io>
2024-10-11 14:07:24 -0400

    SoftAssert

--- src/test/java/tutorial/tests/AssertionsTestNG.java
+++ src/test/java/tutorial/tests/AssertionsTestNG.java
@@ -14,11 +14,13 @@ import org.testng.annotations.Test;
 import java.net.CacheRequest;
 import java.time.Duration;
 
+import static org.testng.Assert.*;
+
 public class AssertionsTestNG extends BasePage {
     String expectedTitle = "Your Store";
 
     @Test
-    public void assertEqual(){
+    public void assertEqualTitle(){
         manager.chromedriver().setup();
         driver =new ChromeDriver();
         driver.manage().window().maximize();
@@ -27,12 +29,35 @@ public class AssertionsTestNG extends BasePage {
         //Getting title
         String getTitle = driver.getTitle();
         //Aseertion
-        Assert.assertEquals(getTitle,expectedTitle);
+         assertEquals(getTitle,expectedTitle,"Title is NOT match"); //static Assert.*
 
     }
 
+    @Test
+    public void example(){
+        driver =new ChromeDriver();
+        String str = " ";
+        Assert.assertNotNull(str);
+    }
+    // Soft Assert
 
+@Test
+public void softAssertEx(){
 
+    manager.chromedriver().setup();
+    driver =new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get(URL);
+    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    String actualTitle = driver.getTitle();
+    assertEquals(actualTitle,expectedTitle);
+    String actualUrl = driver.getCurrentUrl();
+    String expectedUrl = URL;
+    assertEquals(URL,actualUrl);
+    driver.findElement(By.name("search")).sendKeys("HP",Keys.ENTER);
+    assertTrue(driver.findElement(By.linkText("HP LP3065")).isDisplayed());
+
+}
 
 
 
