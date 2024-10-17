eee11669e2d39748531d7ebf0860704b60bbabbe
vikenty19 <vikenty.plakhov@testpro.io>
2024-10-17 15:06:27 -0400

    .

--- src/test/java/tutorial/tests/ObjectDataProvider.java
+++ src/test/java/tutorial/tests/ObjectDataProvider.java
@@ -8,9 +8,17 @@ import java.util.*;
 public class ObjectDataProvider {
     @Test(dataProvider = "HashMap")
     public void test(Object[] map) {
+  //jagged array - different size of every object
+  //    System.out.println(Arrays.toString(map));
+        //another way
+        for (int i = 0; i < map.length ; i++) {
+            System.out.print (map[i]+"  ");
 
-        System.out.println(Arrays.toString(map));
+        }
+        System.out.println();
+        System.out.println("_____________");
     }
+
 
     @DataProvider(name = "HashMap")
     public Iterator<Object[]> data() {
@@ -18,6 +26,7 @@ public class ObjectDataProvider {
 
         data.add(new Object[]{"email", "vinkotov@example.com"});
         data.add(new Object[]{"password", 1234});
+        data.add(new Object[]{56});
         return data.iterator();
 
     }
