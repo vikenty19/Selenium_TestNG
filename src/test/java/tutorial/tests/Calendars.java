b3e6eeab689778ad6674fe314eeabf78888ea381
vikenty19 <vikenty.plakhov@testpro.io>
2024-10-17 12:18:12 -0400

    Data provider aded to calendars

--- src/test/java/tutorial/tests/Calendars.java
+++ src/test/java/tutorial/tests/Calendars.java
@@ -156,15 +156,15 @@ public class Calendars extends BasePage {
 
     }
 
-    @Test
-    public void DropDownCalendar(String selDay,String selMonth,String selYear){
+    @Test(dataProvider = "getData")
+    public void DropDownCalendar(String Day,String Month,String Year){
 
         driver.get("https://www.hyrtutorials.com/p/calendar-practice.html");
         findElement(By.cssSelector("#third_date_picker")).click();
-       selectDataInCalendar("15","Mar","2027");
-       driver.navigate().refresh();
+       selectDataInCalendar(Day,Month,Year);
+     /*  driver.navigate().refresh();
         findElement(By.cssSelector("#third_date_picker")).click();
-        selectDataInCalendar("1","Mar","2025");
+        selectDataInCalendar("1","May","2025");*/
         tearDown();
     }
 
@@ -178,5 +178,10 @@ public class Calendars extends BasePage {
         String eDay = selDay;
         String xpath = ("//table[@class= 'ui-datepicker-calendar']//td[@data-handler='selectDay']/a[text() =\'"+eDay+"\']");
         findElement(By.xpath(xpath)).click();
+    }
+    @DataProvider
+    public Object[][] getData(){
+        String[][] date = {{"15","Mar","2027"},{"1","May","2025"},{"31","Dec","2027"},{"1","Jan","2028"}};
+        return date;
     }
 }
--- src/test/java/tutorial/tests/DataProvider1.java
+++ src/test/java/tutorial/tests/DataProvider1.java
@@ -4,25 +4,32 @@ import com.tutorial.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DataProvider1 extends BasePage {
 
-    @Test
-    public void loginDDTest(){
+    @Test(dataProvider = "setOfData")
+    public void loginDDTest(String email1,String password1){
         driver.get(URL);
         //login
        findElement(By.cssSelector(".fa-user")).click();
        findElement(By.xpath("//li/a[text()='Login']")).click();
         WebElement emailBtn = waitUntilClickable(By.id("input-email"));
         emailBtn.click();
-        emailBtn.sendKeys(email);
+        emailBtn.sendKeys(email1);
         WebElement login =
                 waitUntilClickable(By.id("input-password"));
-        login.sendKeys(password);
+        login.sendKeys(password1);
         findElement(By.cssSelector("[type = 'submit']")).click();
         Assert.assertTrue(waitUntilVisible(By.xpath("//h2[text() = 'My Account']")).isDisplayed());
         tearDown();
 
+    }
+    @DataProvider
+    public Object[][] setOfData() {
+        String[][] data = {{"vv@mail.ru", "megadelta"}, {"amotooricap3@gmail.com", "12345"}
+                , {"amotooricap7@gmail.com", "12345"}};
+        return data;
     }
 }
