5bfa8bea7261256dab6d425a3a4142118177485b
vikenty19 <vikenty.plakhov@testpro.io>
2025-08-15 14:55:20 -0400

    WishList class dependsOn method implyied

--- src/test/java/tutorial/tests/BaseTest.java
+++ src/test/java/tutorial/tests/BaseTest.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
@@ -21,14 +23,14 @@ public class BaseTest {
     protected String email = "vv@mail.ru";
     protected String password = "megadelta";
     protected String passwordTutorial = "12345";
-    @BeforeMethod
+    @BeforeClass
     public void setUpBrowser(){
         manager.chromedriver().setup();
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();
     }
-    @AfterMethod
+    @AfterClass
     public void tearDown(){
         driver.quit();
     }
--- src/test/java/tutorial/tests/WishListTest.java
+++ src/test/java/tutorial/tests/WishListTest.java
@@ -5,16 +5,19 @@ import com.tutorial.LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class WishListTest extends BaseTest{
+
+
     @Test
     public void addProductToWishListAndViewList(){
         driver.get(URL);
         //login
-        LoginPage loginPage = new LoginPage(driver);
         BasePage basePage = new BasePage(driver);
+        LoginPage loginPage = new LoginPage(driver);
         loginPage.openLoginPage();
         loginPage.loginUser(email,password);
         //find product
@@ -27,10 +30,20 @@ public class WishListTest extends BaseTest{
         driver.findElement(By.cssSelector("button>.fa-heart")).click();
         Assert.assertTrue(basePage.waitUntilVisible(By.cssSelector(".alert-success")).isDisplayed());
         //view wish list
-        driver.findElement(By.linkText("wish list")).click();
+      //  driver.findElement(By.linkText("wish list")).click();
+        WebElement wishLink= wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("wish list")));
+        wishLink.click();
         Assert.assertTrue(basePage.waitUntilVisible(By.xpath("//div[@id='content']/h2")).isDisplayed());
         Assert.assertTrue(driver.getTitle().equals("My Wish List"));
 
 
+    }
+    @Test(dependsOnMethods = {"addProductToWishListAndViewList"})
+    public void removeItemFromWishList(){
+        BasePage basePage = new BasePage(driver);
+        WebElement removeBtn = basePage.waitUntilClickable(By.cssSelector(".fa-times"));
+        removeBtn.click();
+        WebElement emptyWishList = basePage.waitUntilVisible(By.xpath("//div[@id = 'content']/p"));
+        Assert.assertEquals(emptyWishList.getText(), "Your wish list is empty.");
     }
 }
