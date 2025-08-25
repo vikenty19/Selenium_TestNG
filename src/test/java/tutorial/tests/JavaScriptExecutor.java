b057cf00a4de49617eb06db28f49feaf70abc695
vikenty19 <vikenty.plakhov@testpro.io>
2025-08-25 16:15:02 -0400

    JavaScript Executor class

--- src/test/java/tutorial/tests/JavaScriptExecutor.java
+++ src/test/java/tutorial/tests/JavaScriptExecutor.java
@@ -1,65 +1,130 @@
 package tutorial.tests;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
+import javax.xml.transform.sax.SAXResult;
+import java.io.File;
+import java.io.IOException;
+
 public class JavaScriptExecutor extends BaseTest {
+    @Test
+    public void javaScriptGetTitle(){
+        JavascriptExecutor jse = ((JavascriptExecutor) driver);
+        driver.get(koelUrl);
+        String title =jse.executeScript("return document.title").toString();
+        String URL = jse.executeScript("return document.URL").toString();
+        System.out.println(title+"  "+URL);
+    }
+    @Test
+    public void lavaScriptEnterText() throws InterruptedException {
+
+        driver.get(koelUrl);
+        WebElement email = driver.findElement(By.cssSelector("form>[type='email']"));
+        javaScriptEnterText(driver,email,"Viktoria@rr.dd");
+        WebElement password = driver.findElement(By.cssSelector("form>[type='password']"));
+        javaScriptEnterText(driver,password,"12345678");
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        javaClick(submit,driver);
+        Thread.sleep(2000);
+
+    }
+    public void javaScriptEnterText(WebDriver driver,WebElement element,String text){
+        JavascriptExecutor jse = ((JavascriptExecutor) driver);
+        jse.executeScript("arguments[0].value = '"+text+"'",element);
+    }
 
     @Test
- public void javaScriptAlert() throws InterruptedException {
+    public void javaScriptAlert() throws InterruptedException {
         JavascriptExecutor jse = ((JavascriptExecutor) driver);
-     driver.get(url);
-     jse.executeScript("alert('DDDDDDDD')");
-     Thread.sleep(2000);
+        driver.get(url);
+        jse.executeScript("alert('DDDDDDDD')");
+        Thread.sleep(2000);
 
 
     }
- @Test
- public void javaScriptPrompt() throws InterruptedException {
-  driver.get(url);
-  JavascriptExecutor jse = ((JavascriptExecutor) driver);
-  jse.executeScript("prompt('Enter your name')");
-  Thread.sleep(2000);
- }
- @Test
- public void javaScriptConfirm() throws InterruptedException {
-  driver.get(url);
-     JavascriptExecutor jse = ((JavascriptExecutor) driver);
-  jse.executeScript("confirm('Do you want to proceed?')");
-  Thread.sleep(2000);
- }
- @Test
+
+    @Test
+    public void javaScriptPrompt() throws InterruptedException {
+        driver.get(url);
+        JavascriptExecutor jse = ((JavascriptExecutor) driver);
+        jse.executeScript("prompt('Enter your name')");
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void javaScriptConfirm() throws InterruptedException {
+        driver.get(url);
+        JavascriptExecutor jse = ((JavascriptExecutor) driver);
+        jse.executeScript("confirm('Do you want to proceed?')");
+        Thread.sleep(2000);
+    }
+
+    @Test
     public void javaScriptClick() throws InterruptedException {
         driver.get(url);
-     JavascriptExecutor jse = ((JavascriptExecutor) driver);
- //    jse.executeScript("document.getElementById('alert1').click()");
-     //another way using Webelement
-     WebElement button = driver.findElement(By.id("alert1"));
-    // jse.executeScript("arguments[0].click()",button);//[0] refer to number of arguments in the line
-     //reusable method click
-     javaClick(button,driver);
-     Thread.sleep(2000);
+        JavascriptExecutor jse = ((JavascriptExecutor) driver);
+        //    jse.executeScript("document.getElementById('alert1').click()");
+        //another way using Webelement
+        WebElement button = driver.findElement(By.id("alert1"));
+        // jse.executeScript("arguments[0].click()",button);//[0] refer to number of arguments in the line
+        //reusable method click
+        javaClick(button, driver);
+        Thread.sleep(2000);
 
- }
- @Test
+    }
+
+    @Test
     public void javaScriptFlashing() throws InterruptedException {
         driver.get(URL);
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        WebElement cartButton =driver.findElement(By.cssSelector("#cart>button"));
+        WebElement cartButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#cart>button")));
         //default color
-        String defaultColor = cartButton.getCssValue("background-color");
+ /*       String defaultColor = cartButton.getCssValue("background-color");
         for (int i=0; i<=20;i++){
          jse.executeScript("document.querySelector('#cart>button').style.background='green';");
          Thread.sleep(50);
             jse.executeScript("document.querySelector('#cart>button').style.background='"+defaultColor+"';");
+        }*/
+        javaScriptFlash(driver, cartButton);
+
+    }
+    @Test
+    public void javaScriptBorderAndScreenShot() throws InterruptedException {
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        driver.get(koelUrl);
+        WebElement regForm = driver.findElement(By.cssSelector("form[data-testid='login-form']"));
+        jse.executeScript("arguments[0].style.border='5px solid red'",regForm);
+
+
+         File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileHandler.copy(srcFile,new File("./KoelScreenshots/highlitedBorder.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
- }
+        jse.executeScript("arguments[0].style.border=''",regForm);
 
-public void javaClick(WebElement element, WebDriver driver){
-    JavascriptExecutor jse = (JavascriptExecutor) driver;
-    jse.executeScript("arguments[0].click()",element);
-}
+    }
+
+    public void javaClick(WebElement element, WebDriver driver) {
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("arguments[0].click()", element);
+    }
+
+    public void javaScriptFlash(WebDriver driver, WebElement element) {
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        String defaultColor = element.getCssValue("background-color");
+        for (int i = 0; i <= 20; i++) {
+            jse.executeScript("arguments[0].style.background='green';", element);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            jse.executeScript("arguments[0].style.background='" + defaultColor + "';", element);
+        }
+    }
 }
